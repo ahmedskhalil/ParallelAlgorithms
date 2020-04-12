e38e3f937addca28fa9e5f52df069b7a31aa0220
Ahmed Khalil <ahmed.bn.khalil@gmail.com>
2020-04-12 19:50:31 +0200

    adding pthreading solution

--- parallelised_quicksort.cpp
+++ parallelised_quicksort.cpp
@@ -16,10 +16,12 @@
 
 #include <time.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 
 
-#define NUM         1000000000
+#define NUM         10000000
+#define TH_LVL      10
 
 void swap(double lst[], int x, int y);
 int partitioner(double lst[], int first, int last);
@@ -28,10 +30,13 @@ void helper(double lst[], int first, int last);
 void qsort(double lst[], int size);
 int isSorted(double lst[], int size);
 
+void  pqsort(double lst[], int size, int thlvl);
+void* phelper(void* tharg) __attribute__ ((noreturn));
+
 
 
 struct th_data {
-    double* lst; int fst; int dlst; int lv;
+    double* dlst; int fst; int lst; int lv;
 };
 
 int doublesComparer(const void* x, const void* b);
@@ -62,13 +67,27 @@ int main(int argc, char* argv[]){
     qsort(dlst, NUM0);
     gettimeofday(&end, NULL);
 
+    if (!isSorted(dlst, NUM0)) printf("ALERT: not sorted!\n");
+
     dur = (  (end.tv_sec * 1000000   + end.tv_usec)
            - (start.tv_sec * 1000000 + start.tv_usec)
           )/ 1000000.0;
     printf("Ordinary quicksort processed in: %lf sec.\n", dur);
 
 
+    // parallel qsort benchmarking
+    memcpy(dlst, dlstonhold, NUM0 * sizeof(double));
 
+    gettimeofday(&start, NULL);
+    pqsort(dlst, NUM0, TH_LVL);
+    gettimeofday(&end, NULL);
+
+    if (!isSorted(dlst, NUM0)) printf("ALERT: not sorted!\n");
+
+    dur = (  (end.tv_sec * 1000000   + end.tv_usec)
+           - (start.tv_sec * 1000000 + start.tv_usec)
+          )/ 1000000.0;
+    printf("Parallelised quicksort processed in: %lf sec.\n", dur);
 
     return 0;
 }
@@ -118,5 +137,73 @@ int isSorted(double lst[], int size){
     return 1; // ascendingly sorted
 }
 
-//void  pqsort(double lst[], int size, int thlevel){}
-//void* phelper(void* threadarg) __attribute__ ((noreturn)){}
+void  pqsort(double dlst[], int size, int thlvl){
+    int cval;
+    void* status;
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    struct th_data thd;
+    thd.dlst = dlst;
+    thd.fst  = 0;
+    thd.lst  = size - 1;
+    thd.lv   = thlvl;
+
+    pthread_t toplvl_th;
+    cval = pthread_create(&toplvl_th, &attr, phelper, (void*)&thd);
+    if(cval) { printf("ERROR: failed to create thread\n"); exit(-1); }
+
+    pthread_attr_destroy(&attr);
+    cval = pthread_join(toplvl_th, &status);
+    if(cval) { printf("ERROR: failed to join thread\n"); exit(-1); }
+}
+
+
+void* phelper(void* tharg){
+    int t, cval, mid;
+    void* status;
+
+    struct th_data* thdata;
+    thdata = (struct th_data*) tharg;
+
+    if (thdata->lv <= 0 || thdata->fst == thdata->lst+1){
+        helper(thdata->dlst, thdata->fst, thdata->lst);
+        pthread_exit(NULL);
+    }
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    mid = partitioner(thdata->dlst, thdata->fst, thdata->lst);
+
+    struct th_data th_data_array[2];
+
+    for (t=0; t<2; t++){
+        th_data_array[t].dlst = thdata->dlst;
+        th_data_array[t].lv   = thdata->lv - 1;
+    }
+
+    th_data_array[0].fst = thdata->fst;
+    th_data_array[0].lst = mid - 1;
+    th_data_array[1].fst = mid + 1;
+    th_data_array[1].lst = thdata->lst;
+
+    pthread_t threads[2];
+    for (t=0; t<2; t++){
+        cval = pthread_create(&threads[t], &attr, phelper, 
+                                (void*) &th_data_array[t]);
+
+        if(cval) { printf("ERROR: failed to create thread\n"); exit(-1); }
+    }
+
+    pthread_attr_destroy(&attr);
+    // joining left and right
+    for (t=0; t<2; t++){
+        cval = pthread_join(threads[t], &status);
+        if(cval) { printf("ERROR: failed to join thread\n"); exit(-1); }
+    }
+    pthread_exit(NULL);
+}
